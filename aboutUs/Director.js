673dfa86c9588b6e5d89c165755e3df72584b287
David Bille <d_bille@yahoo.com>
2019-10-26 12:54:56 -0500

    adding JS file

---
+++ aboutUs/Director.js
@@ -0,0 +1,10 @@
+class Director {
+  constructor(name, title, phone, email) {
+    this.name = name;
+    this.title = title;
+    this.phone = phone;
+    this.email = email;
+  }
+}
+
+module.exports = Director;
--- aboutUs/app.js
+++ aboutUs/app.js
@@ -1,3 +1,27 @@
+// let Director = require('./Director.js') // not working for some reason?
+
+// can remove this once modcule using require is working
 class Director {
-  
+  constructor(name, title, phone, email) {
+    this.name = name;
+    this.title = title;
+    this.phone = phone;
+    this.email = email;
+  }
 }
+
+const babette = new Director('Babette Cohn', 'Director of Education & Programming', '301-593-3410 x106', 'bcohn@shaaretefila.org')
+const shana = new Director('Shana Rosenblatt', 'Executive Director', '301-593-3410 x101', 'srosenblatt@shaaretefila.org')
+const hazzan = new Director('Hazzan Adina Klein Ackerman', 'Cantor', '301-593-3410', 'caackerman@shaaretefila.org')
+const beth = new Director('Beth Adler', 'Early Childhood Center Director', '301-593-3410 x105', 'badler@shaaretefila.org')
+
+const directors = []
+directors.push(babette)
+directors.push(shana)
+directors.push(hazzan)
+directors.push(beth)
+
+
+directors.forEach((elem) => {
+  console.log(`Name: ${elem.name}, Title: ${elem.title}, Phone: ${elem.phone}, Email: ${elem.email}`)
+})
