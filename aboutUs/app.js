6363a5ea11dad7299fad9e26167e08ef3acf9ab1
David Bille <d_bille@yahoo.com>
2019-10-26 13:48:13 -0500

    JS just needs to allow for images to be added to appropriate person

--- aboutUs/app.js
+++ aboutUs/app.js
@@ -2,26 +2,65 @@
 
 // can remove this once modcule using require is working
 class Director {
-  constructor(name, title, phone, email) {
+  constructor(name, title, phone, email, photo) {
     this.name = name;
     this.title = title;
     this.phone = phone;
     this.email = email;
+    this.photo = photo;
   }
 }
 
-const babette = new Director('Babette Cohn', 'Director of Education & Programming', '301-593-3410 x106', 'bcohn@shaaretefila.org')
-const shana = new Director('Shana Rosenblatt', 'Executive Director', '301-593-3410 x101', 'srosenblatt@shaaretefila.org')
-const hazzan = new Director('Hazzan Adina Klein Ackerman', 'Cantor', '301-593-3410', 'caackerman@shaaretefila.org')
-const beth = new Director('Beth Adler', 'Early Childhood Center Director', '301-593-3410 x105', 'badler@shaaretefila.org')
+const babette = new Director('Babette Cohn', 'Director of Education & Programming', '301-593-3410 x106', 'bcohn@shaaretefila.org', 'babette-cohn.jpg')
+const shana = new Director('Shana Rosenblatt', 'Executive Director', '301-593-3410 x101', 'srosenblatt@shaaretefila.org', './images/shana-rosenblatt.jpg')
+const hazzan = new Director('Hazzan Adina Klein Ackerman', 'Cantor', '301-593-3410', 'caackerman@shaaretefila.org', './images/hazzan-ackerman.jpg')
+const beth = new Director('Beth Adler', 'Early Childhood Center Director', '301-593-3410 x105', 'badler@shaaretefila.org','./Images/beth-adler.jpg')
 
 const directors = []
 directors.push(babette)
 directors.push(shana)
 directors.push(hazzan)
 directors.push(beth)
+console.log(directors)
 
 
 directors.forEach((elem) => {
-  console.log(`Name: ${elem.name}, Title: ${elem.title}, Phone: ${elem.phone}, Email: ${elem.email}`)
+  const card = document.createElement('div')
+  card.setAttribute('class', 'card')
+  
+  
+  let img = document.createElement('img')
+  img.setAttribute('class', 'card-img-top')
+  img.src = ''
+
+  let cardBody = document.createElement('div')
+  cardBody.setAttribute('class', 'card-body')
+
+  let h5 = document.createElement('h5')
+  h5.setAttribute('class', 'card-title')
+  h5.innerHTML = elem.name;
+
+  let p = document.createElement('p')
+  p.setAttribute('class', 'card-text')
+  p.innerHTML = `
+  <b>${elem.title}</b><br>
+  ${elem.phone}<br>
+  ${elem.email}
+  `
+  document.querySelector('#cardWrap').append(card)
+  card.append(img)
+  card.append(cardBody)
+  cardBody.appendChild(h5)
+  cardBody.appendChild(p)
+  
+
+
+  // document.querySelector('.card-title').innerHTML = elem.name;
+  // document.querySelector('.card-text').innerHTML = 
+  // `
+  // <b>${elem.title}</b><br>
+  // ${elem.phone}<br>
+  // ${elem.email}
+  // `;
+
 })
